d77cc75c4e9a31252b6522df8a04cad52ab9847d
Mostafa Reda <msreda224@gmail.com>
2021-04-03 10:36:35 +0200

    MS

--- app/src/main/java/com/example/mymooc/CustomLogic.java
+++ app/src/main/java/com/example/mymooc/CustomLogic.java
@@ -11,6 +11,7 @@ public class CustomLogic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_logic);
+    // TODO das f sdf asdf asdf
     }
 
 
